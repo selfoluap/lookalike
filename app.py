921511e3354eed3cf423258ed170633494bb7e0a
Paulo Liedtke <liedtke.paulo@gmail.com>
2023-06-09 20:13:59 +0200

    Feat: image upload post

--- app.py
+++ app.py
@@ -1,29 +1,18 @@
 from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
-import db
 
 app = Flask(__name__)
 api = Api(app)
 
 class RESTApi(Resource):
     def post(self):
-        if 'image' not in request.files:
-            return jsonify(message="No image file found")
-        image = request.files['image']
-        print(image)
-        return jsonify(message="POST request received")
-
-
-class StoreApi(Resource):
-    def post(self):
-        if 'image' not in request.files:
-            return jsonify(message="No image file found")
-        image = request.files['image']
-        db.store_image(image)
-        return jsonify(message="POST request received")
+        data = request.get_json()
+        if 'image' not in data:
+            return jsonify(message="No image provided"), 400
+        image = data['image']
+        return jsonify(message="Image uploaded successfully")
 
 api.add_resource(RESTApi, '/api')
-api.add_resource(StoreApi, '/store')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- frontend.py
+++ frontend.py
@@ -1,25 +1,29 @@
 import streamlit as st
 import requests
-from PIL import Image
 from io import BytesIO
+import base64
+from PIL import Image
 
 st.title("Image Uploader")
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png", "jpeg"])
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
+    image = image.convert("RGB")  # Convert to RGB mode
+    buffered = BytesIO()
+    image.save(buffered, format="JPEG")
+    image_bytes = buffered.getvalue()
+    img_b64 = base64.b64encode(image_bytes).decode('utf-8')
     st.image(image, caption="Uploaded Image", use_column_width=True)
-
 
 if st.button("Search Image"):
     url = "http://localhost:5000/api"
-    payload = {"key": "value"}
 
+    payload = {'image': img_b64}
 
     with st.spinner("Searching Image..."):
         try:
             response = requests.post(url, json=payload)
-
             if response.status_code == 200:
                 st.success(f"POST request successful: {response.json()}")
             else:
